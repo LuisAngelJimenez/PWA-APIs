4a7cd12fd469651861a9107827a5c45f97549610
Luis Jimenez <luisangelastellanos003@gmail.com>
2024-10-17 09:43:47 -0600

    feat(api): making url for API

--- src/app/features/usingApi/usingApi.component.ts
+++ src/app/features/usingApi/usingApi.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { APIService } from '../../services/API.service';
+import { ResponseKanye } from '../../data/interfaces/response';
 
 @Component({
   selector: 'app-using-api',
@@ -10,17 +11,17 @@ import { APIService } from '../../services/API.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class UsingApiComponent {
-  apiData: any;
+  public quote = signal<ResponseKanye | null>(null);
 
   constructor(private AS: APIService) {
     this.ObtainingData();
   }
 
-  ObtainingData(){
+  public ObtainingData() {
     this.AS.getData().subscribe(
-      (data) => {
-        this.apiData = data
-        console.log(this.apiData);
+      ( data: ResponseKanye ) => {
+        console.log("tu frase de kanye del dia: " +data.quote);
+        this.quote.set(data);
       },
     );
   }
--- src/app/services/API.service.ts
+++ src/app/services/API.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { ResponseKanye } from '../data/interfaces/response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class APIService {
-  constructor(private http:HttpClient) {
-  }
-  getData():Observable<any>{
-    const url='https://api.kanye.rest';
-    return this.http.get<any>(url);
+  private url = 'https://api.kanye.rest';
+
+  constructor(private http:HttpClient) {}
+
+  public getData() {
+    return this.http.get<ResponseKanye>(this.url);
   }
 }
